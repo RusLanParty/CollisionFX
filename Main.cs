45f12ecf34c5890bc13e4a65c815fababf12a587
Ruslan Libin <ruslanlib97@gmail.com>
2022-10-14 10:10:27 +0300

    Fixes

--- Main.cs
+++ Main.cs
@@ -9,28 +9,26 @@ using GTA.Math;
 using GTA.Native;
 using static System.TimeZoneInfo;
 
-namespace CrashBlur
+namespace CollisionFX
 {
     public class Main : Script
     {
         Vehicle car;
         float speed;
         float afterSpeed;
-        int time;
-        int aftertime;
-        int res;
-        bool skiptime=false;
         float force;
-        float i;
+        int i;
         float mod;
-        float tth=0;
+        float tth;
         float dif;
-        bool inRec=false;
         float inRecColSpeed;
         float inRecAfterColSpeed;
         float inRecDifSpd;
         int camType;
         bool fp;
+        double flashThresh;
+        double effectThresh;
+        int effectType;
         
 
 
@@ -38,125 +36,148 @@ public Main()
         {
             camType = Settings.GetValue("SETTINGS", "ShakeType", 0);
             fp = Settings.GetValue("SETTINGS", "FirstPersonOnly", false);
+            flashThresh = Settings.GetValue("SETTINGS", "FlashThreshold", 1.0f);
+            effectThresh = Settings.GetValue("SETTINGS", "BlurThreshold", 2.0f);
+            effectType = Settings.GetValue("SETTINGS", "effectType", 0);
             Tick += onTick;
 
         }
 
+        private void flash()
+        {
+            //FLASH
+
+
+            Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "PlayerSwitchPulse");
+            i = 0;
+            while (i < 1)
+            {
+                Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 1.0f);
+                Wait(0);
+                i++;
+            }
+            while (i > 0)
+            {
+                Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 0.0f);
+                Wait(0);
+                i--;
+            }
+
+            //\FLASH
+        }
         public void effect()
         {
             Ped player = Game.Player.Character;
 
             car = player.CurrentVehicle;
-
-            if ((!skiptime) && (!inRec))
-            {
+         
                 speed = car.Speed;
-                time = Game.GameTime;
-                skiptime = true;
-            }
-            aftertime = Game.GameTime;
-            res = aftertime - time;
-            if (res > 2)
+                force = 0;
+
+            if (car.HasCollided)
             {
-                skiptime = false;
-                inRec = false;
-                afterSpeed = car.Speed;
-                force = speed - afterSpeed;
+                Wait(6);
+                    afterSpeed = car.Speed;
+                    force = (int)speed - (int)afterSpeed;
 
-                if (force < 0)
-                {
-                    force = force * -1;
-                }
-
+                    if (force < 0)
+                    {
+                        force = force * -1;
+                    }
             }
-            if ((force > 2) && (car.HasCollided) || (tth > 0))
+           
+            if (force > flashThresh)
+            {
+                flash();
+            }
+
+
+            if (force > effectThresh)
             {
 
-                //FLASH
+                mod = force / 50;
 
-
-                Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "PlayerSwitchPulse");
-                i = 0;
-                while (i < 1)
+                if (mod > 1.0f)
                 {
-                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 1.0f);
-                    Wait(0);
-                    i++;
-                }
-                while (i > 0)
-                {
-                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 0.0f);
-                    Wait(0);
-                    i--;
-                }
-
-                //\FLASH
-
-
-                if (force > 2 || (tth > 0))
-                {
-
-                    mod = force / 50;
-                    if (mod > 1.0f)
-                    {
-                        mod = 1.0f;
-                    }
-                    GTA.GameplayCamera.StopShaking();
-                    if (camType == 0) { GTA.GameplayCamera.Shake(CameraShake.DeathFail, mod); }
-                    if (camType == 1) { GTA.GameplayCamera.Shake(CameraShake.SmallExplosion, mod); }
-                    if (camType == 2) { GTA.GameplayCamera.Shake(CameraShake.Jolt, mod); }
-
-                    force = force * 15;
-                    if (force >= 200)
-                    {
-                        force = 200;
-                    }
-
-                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "NG_filmic18");
-
-                    //BLUR
-                    i = 0;
-                    while (i <= 2)
-                    {
-                        Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, (float)i / 3);
-                        Wait(0);
-                        i++;
-                    }
-
-                    force = force + tth;
-
-                    i = 0;
                     mod = 1.0f;
+                }
 
-                    while ((force >= i) && (!inRec))
+                GTA.GameplayCamera.StopShaking();
+
+                if (camType == 0) { GTA.GameplayCamera.Shake(CameraShake.DeathFail, mod); }
+                if (camType == 1) { GTA.GameplayCamera.Shake(CameraShake.SmallExplosion, mod); }
+                if (camType == 2) { GTA.GameplayCamera.Shake(CameraShake.Jolt, mod); }
+
+                force = force * 15;
+
+                if (force >= 200)
+                {
+                    force = 200;
+                }
+
+                switch (effectType)
+                {
+                    case 0:
+                        Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "NG_filmic18");
+                         break;
+
+                    case 1:
+                        Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "hud_def_blur");
+                        break;
+                }
+                
+
+                //BLUR
+
+                /* 
+                     i = 0;
+                     while (i <= 2)
+                 {
+                     Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, (float)i / 3);
+                     Wait(0);
+                     i++;
+                 }
+                */
+
+                i = 0;
+                mod = 1.0f;
+                tth = force;
+                dif = 1 / tth;
+
+                while (force >= i)
+                {
+                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, (float)mod);
+                    tth = force - i;
+                    Wait(1);
+                    mod = mod - dif;
+                    i++;
+                    inRecColSpeed = car.Speed;
+
+                    if (mod < 0)
                     {
-                        tth = (int)force - (int)i;
+                        mod = 0;
+                    }
 
-                        Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, (float)mod);
-                        Wait(1);
-                        dif = 1 / tth;
+                   // GTA.UI.Screen.ShowHelpText("Effect: " + mod + " TTH: " + tth + " DIF: " + dif + " Force: " + force, 5000, false, false);
 
-                        mod = mod - dif;
-                        i++;
-                        // GTA.UI.Screen.ShowSubtitle("You're ~r~injured~s~ (inside a car), recovering: ~b~" + tth + " ~s~MODIFIER STR: ~r~" + mod, 3000);
-                        inRecColSpeed = car.Speed;
-                        if (car.HasCollided)
+                    if (car.HasCollided)
+                    {
+                        Wait(2);
+                        inRecAfterColSpeed = car.Speed;
+                        inRecDifSpd = inRecColSpeed - inRecAfterColSpeed;
+
+                        if (inRecDifSpd < 0)
                         {
-                            Wait(1);
-                            inRecAfterColSpeed = car.Speed;
-                            inRecDifSpd = inRecColSpeed - inRecAfterColSpeed;
-                            if (inRecDifSpd < 0)
-                            {
-                                inRecDifSpd = inRecDifSpd * -1;
-                            }
-                            if (inRecDifSpd > 2)
-                            {
-                                i = 0;
-                                inRec = true;
-                                speed = car.Speed;
-                                time = Game.GameTime;
-                                mod = 1.0f;
-                            }
+                            inRecDifSpd = inRecDifSpd * -1;
+                        }
+
+                        if (inRecDifSpd > effectThresh)
+                        {
+                            inRecDifSpd = inRecDifSpd * 10;
+                            force = inRecDifSpd + tth;
+                            mod = 1.0f;
+                            dif = 1 / force;
+                            i = 0;
                         }
                     }
                 }
@@ -187,7 +208,6 @@ public Main()
                 i = 0;
                 tth = 0;
                 force = 0;
-                inRec = false;
                 
             }
         }
