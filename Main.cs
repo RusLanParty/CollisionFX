6bce05c3224b41ffed6d391c6ea3f48abbecbdca
Ruslan Libin <ruslanlib97@gmail.com>
2023-10-24 12:58:42 +0300

    Rewritten from scratch

--- Main.cs
+++ Main.cs
@@ -13,199 +13,53 @@ namespace CollisionFX
 {
     public class Main : Script
     {
-        Vehicle car;
-        float speed;
-        float afterSpeed;
-        float force;
-        int i;
-        float mod;
-        float tth;
-        float dif;
-        float inRecColSpeed;
-        float inRecAfterColSpeed;
-        float inRecDifSpd;
-        int camType;
-        bool fp;
-        double flashThresh;
-        double effectThresh;
-        int effectType;
-        bool flashEnabled;
+        float oldSpeed = 0.0f;
+        bool fadedOut = true;
+        float modifier = 0.0f;
+        int frameSkip = 0;
 
-
-
-public Main()
-        {
-            camType = Settings.GetValue("SETTINGS", "ShakeType", 0);
-            fp = Settings.GetValue("SETTINGS", "FirstPersonOnly", false);
-            flashThresh = Settings.GetValue("SETTINGS", "FlashThreshold", 1.0f);
-            effectThresh = Settings.GetValue("SETTINGS", "BlurThreshold", 2.0f);
-            effectType = Settings.GetValue("SETTINGS", "effectType", 2);
-            flashEnabled = Settings.GetValue("SETTINGS", "flash", true);
+        public Main()
+        {            
             Tick += onTick;
-
         }
-
-        private void flash()
-        {
-            //FLASH
-
-
-            Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "PlayerSwitchPulse");
-            i = 0;
-            while (i < 1)
-            {
-                Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 1.0f);
-                Wait(0);
-                i++;
-            }
-            while (i > 0)
-            {
-                Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, 0.0f);
-                Wait(0);
-                i--;
-            }
-
-            //\FLASH
-        }
-        public void effect()
-        {
-            Ped player = Game.Player.Character;
-
-            car = player.CurrentVehicle;
-         
-                speed = car.Speed;
-                force = 0;
-
-            if (car.HasCollided)
-            {
-                Wait(6);
-                    afterSpeed = car.Speed;
-                    force = (int)speed - (int)afterSpeed;
-
-                    if (force < 0)
-                    {
-                        force = force * -1;
-                    }
-            }
-           
-            if (force > flashThresh && flashEnabled)
-            {
-                flash();
-            }
-
-
-            if (force > effectThresh)
-            {
-
-                mod = force / 50;
-
-                if (mod > 1.0f)
-                {
-                    mod = 1.0f;
-                }
-
-                GTA.GameplayCamera.StopShaking();
-
-                if (camType == 0) { GTA.GameplayCamera.Shake(CameraShake.DeathFail, mod); }
-                if (camType == 1) { GTA.GameplayCamera.Shake(CameraShake.SmallExplosion, mod); }
-                if (camType == 2) { GTA.GameplayCamera.Shake(CameraShake.Jolt, mod); }
-
-                force = force * 15;
-
-                if (force >= 200)
-                {
-                    force = 200;
-                }
-
-                switch (effectType)
-                {
-                    case 0:
-                        Function.Call(Hash._CLEAR_EXTRA_TIMECYCLE_MODIFIER);
-                        Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "NG_filmic18");
-                         break;
-
-                    case 1:
-                        Function.Call(Hash._CLEAR_EXTRA_TIMECYCLE_MODIFIER);
-                        Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "hud_def_blur");
-                        break;
-                    case 2:
-                        Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "NG_filmic18");
-                        Function.Call(Hash._SET_EXTRA_TIMECYCLE_MODIFIER, "hud_def_blur");
-                        break;
-                }
-              
-                i = 0;
-                mod = 1.0f;
-                tth = force;
-                dif = 1 / tth;
-                
-                while (force >= i)
-                {
-                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, (float)mod);
-                    Function.Call(Hash._SET_EXTRA_TIMECYCLE_MODIFIER_STRENGTH, (float)mod);
-                    tth = force - i;
-                    Wait(1);
-                    mod = mod - dif;
-                    i++;
-                    inRecColSpeed = car.Speed;
-
-                    if (mod < 0)
-                    {
-                        mod = 0;
-                    }
-
-                   // GTA.UI.Screen.ShowHelpText("Effect: " + mod + " TTH: " + tth + " DIF: " + dif + " Force: " + force, 5000, false, false);
-
-                    if (car.HasCollided)
-                    {
-                        Wait(2);
-                        inRecAfterColSpeed = car.Speed;
-                        inRecDifSpd = inRecColSpeed - inRecAfterColSpeed;
-
-                        if (inRecDifSpd < 0)
-                        {
-                            inRecDifSpd = inRecDifSpd * -1;
-                        }
-
-                        if (inRecDifSpd > effectThresh)
-                        {
-                            inRecDifSpd = inRecDifSpd * 10;
-                            force = inRecDifSpd + tth;
-                            mod = 1.0f;
-                            dif = 1 / force;
-                            i = 0;
-                        }
-                    }
-                }
-            }
-        }
-
         public void onTick(object sender, EventArgs e)
         {
             Ped player = Game.Player.Character;
-           
-            
+
             if (player.IsInVehicle())
             {
-                if (fp)
-                    camType = Function.Call<int>(Hash.GET_FOLLOW_VEHICLE_CAM_VIEW_MODE);
-                {
-                    if (camType == 4) 
-                    {
-                        effect();
-                    }
-                }    if (!fp)
-                {
-                    effect();
+                Vehicle veh = player.CurrentVehicle;
+                float deltaSpeed = veh.Speed - oldSpeed;
+                if(frameSkip > 3)
+                {                    
+                    oldSpeed = veh.Speed;
                 }
-               
-            } else
-            {
-                i = 0;
-                tth = 0;
-                force = 0;
-                
+                else
+                {
+                    frameSkip++;
+                }
+                if(Math.Abs(deltaSpeed) > 1.0f && fadedOut)
+                {
+                    modifier = Math.Abs(deltaSpeed) * 2;
+                    if (modifier > 1.0f)
+                    {
+                        modifier = 1.0f;
+                    }                    
+                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER, "NG_filmic18");                    
+                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, modifier);
+                    fadedOut = false;
+                }
+                else if (!fadedOut)
+                {
+                    modifier -= 0.01f;
+                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER_STRENGTH, modifier);
+                    if (modifier <= 0.0f)
+                    {
+                        modifier = 0.0f;
+                        fadedOut = true;
+                    }
+                }
             }
-        }
+        }       
     }
 }
